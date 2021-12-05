ce494d44e038d367f531a69a1598205acaa40e1d
Toushif <toushif.haq@gmail.com>
2021-12-05 06:00:15 +0530

    webpack dev server configured with html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: path.resolve(__dirname, "src/index.js"),
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, "./dist"),
     },
     module: {
         rules: [
@@ -13,8 +14,18 @@ module.exports = {
                 test: /\.js|[x]$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"],
-            },
+            }
         ],
     },
-    devtool: "source-map"
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+    ],
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: "./dist",
+        },
+    },
 };
